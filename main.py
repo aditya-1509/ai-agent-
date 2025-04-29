d6d98cd662facbd4a0d5940246134c4f08fdfc77
aditya1000007 <adityarani.1509@gmail.com>
2025-04-29 10:23:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os 
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
+
+
+mcp_fetc_server = MCPServerStdio(
+    command = "python",
+    args = ["-m","mcp_server_fetch"]
+)
+os.environ["GROQ_API_KEY"]="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"
+
+agent = Agent(
+    model="groq:llama-3.3-70b-versatile",
+    mcp_server=[mcp_fetc_server]
+)
+
+async def main():
+    async with agent.run_mcp_servers():
+        result = await agent.run("extract the content and summarize it: https://www.drishtiias.com/daily-updates/daily-news-analysis/india-us-relations-5")
+        output = result.output
+        return output
+    
+if __name__ == "__main__":
+    output = asyncio.run(main())
+    print(output)
